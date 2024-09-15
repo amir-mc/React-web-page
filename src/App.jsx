f91d60a87b877ab3c8aeec5a017b0719ff673cd3
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-09-15 18:28:21 +0330

    Update App.jsx
    callback handler jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,67 @@
+
+
 import List from "./components/list";
 //import Item from "./components/Item";
 import Search from "./components/search";
-
 
 const title = {
   number:'hello',
   titlee:'act pages'
 };
-
-const combolist = [
-  {
-  id: 1,
-  name:'ali',
-  title:'react pages',
-  number:55,
-  skill:20,
-  URL:'HTTPS://z.com'
-},
-{
-  id: 2,
-  name:'javad',
-  title:'laravel pages',
-  number:33,
-  skill:18,
-  URL:'HTTPS://x.com'
-}
-
-];
-
 const App = () => {
 
+  const H1 = (mamad) =>{
+    mamad='155';
+     
+    console.log(mamad)
+
+  }
+
+  const handelBack = (event) =>{  
+    console.log(event.target.value)
+
+  }
+  
+  const stor = [
+    {
+    id: 1,
+    name:'alii',
+    title:'react pages',
+    number:55,
+    skill:20,
+    URL:'HTTPS://z.com'
+  },
+  {
+    id: 2,
+    name:'javad',
+    title:'laravel pages',
+    number:33,
+    skill:18,
+    URL:'HTTPS://x.com'
+  }
+  
+  
+
+
+  ];
  
 return (
   <div>
-
   <h1>
    {title.number} {title.titlee}
   </h1>
-  <Search/>
-  <List/>
- 
-
+  <H1/>
+  <Search seaechs={handelBack}/>
+  <List list={stor} javd={1220}/>
+  <span>
+    
+  </span>
   </div>
-  
-  
-  
 )
  
 }
-const List = (props) => {
-  return(
-    <ul>
-    {
-      props.combolist.map((item){
-        return <li key={item.id}>
-          
-          <span>
-            <a href={item.URL}> {item.name} </a>
-          </span>
-          <span>
-            {
-              item.skill
-            }
-          </span>
-          <span>
-            {
-              item.title
-            }
-          </span>
-        
-        </li>
-      })
-    }
-  </ul>
+ 
 
-  )
-}
-const Search = () => {
-  const handle = (event)=> {
-    console.log(event.target.value);
-  }
-  return(
-<div>
-<label htmlFor="search">Search</label>
-  <input onchange={handle} type="text" id="search" />
 
-</div>    
-  )
-}
 
 export default App
