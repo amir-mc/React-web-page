ac6e41e74646cf0b5a3745e4a9096703af633ced
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-12 14:26:24 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,16 @@
 
-
-import { useEffect, useReducer, useState } from "react";
-import List from "./components/list";
-import Search from "./components/search";
-
-import useStateStor from "./hooks/useStoragestate";
-
-const title = {
-  number:'hello',
-  titlee:'act pages'
-};
-
-const storyReduser=(state,action)=>{
-  switch (action.type) {
-    case 'SET_STOR_FETCH':
-      return {...state,isLoding:true,isErorr:false
-      }
-      case 'SET_STOR_SUCSSES':
-      return {...state,isLoding:false,isErorr:false,data:action.payload
-      }
-      case 'SET_STOR_ERROR':
-      return {...state,isLoding:false,isErorr:true
-      }
+import usePgae from './useState'
 
 
-      case 'REMOVE_STOR':
-      return {...state,
-         data: state.data.filter((stor)=> stor.id !== action.payload),    }
-    default:
-      return state
-  }
-}
-
-const App = () => {
-
- 
-  const [stories , dipacthStor]=useReducer(storyReduser,{
-    data:[], isLoding:false , isErorr:false
-  })
-  //const [stories , Setsories]=useState([])
-  
-  
-  const [searchTerm , updateSerach]=useStateStor('search','')
-
-  const API='https://react-mini-projects-api.classbon.com/story/list';
-
-  useEffect(()=>{
-      if(!searchTerm) return;
-
-    dipacthStor({type:'SET_STOR_FETCH'})
-    fetch(`${API}?query=${searchTerm}`).then((response)=>response.json()).then((stories)=>{
-      dipacthStor({type:'SET_STOR_SUCSSES' , payload: stories})
-
-    })
-    .catch(()=>dipacthStor({type:'SET_STOR_ERROR'}))
-    
-  },[searchTerm])
-
-    const HandelRemov=(id)=>{ 
-      dipacthStor({type:'REMOVE_STOR' ,payload:id })
-    }
-
-  //const serachStory =stories.data.filter((stor)=>stor.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
-  
-  
-  const handelBack = (event) =>{  
-    updateSerach(event.target.value)
- 
-  }
-   
-
-return (
-  <div>
-  <h1>
-   {title.number} {title.titlee}
-  </h1>
-
-{
-  stories.isErorr && (<p>ERRRRRROR</p>)
-}
-
- {
-  stories.isLoding ?(<p> lodingg </p>):
-  (<Search type="text" id="Search" value="search" label="search" seaechs={handelBack} titleSearch={searchTerm} autoFo/>)
- }
-  
-  <List list={stories.data}  HandelRemo={HandelRemov}/>
-
-  <span>
-    
-  </span>
+const url='https://react-mini-projects-api.classbon.com/Programmer/programmers'
+function App() {
+  const [javad,ahmad]=usePgae(url , 3)
+  return(
+  <div className="conainer">
+    هلو بیب
   </div>
-)
- 
+  )
+  
 }
- 
-
-
 
 export default App
