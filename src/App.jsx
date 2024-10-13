353ebe793ce2602b83584c1c929b34dbadd4930b
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-13 12:04:42 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 
+import { useEffect, useState } from 'react'
 import usePgae from './useState'
+import Card from './component/Card'
+import Pageing from './component/Pageing'
 
 
 const url='https://react-mini-projects-api.classbon.com/Programmer/programmers'
 function App() {
-  const [javad,ahmad]=usePgae(url , 3)
+  const [loding,data]=usePgae(url , 1)
+  const [page,setpage]=useState(1)
+  const [program,setprogram]=useState([])
+  useEffect(()=>{
+    if(loding) return 
+    setprogram(data[page-1  ])
+  },[loding,page])
   return(
-  <div className="conainer">
-    هلو بیب
+  <div className="conainer pt-5">
+    {
+      loding&&(
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border"></div>
+        </div>
+      )
+    }   
+    {
+      !loding&&(
+        <>
+        <div className="row d-flex jutify-content-center">
+          {
+            program.map(({id,...programer})=>{
+              return(
+                <div className="col-3" key={id}>
+                  <Card {...programer}/>
+                </div> 
+              )})
+          }
+        </div>
+        <div className="row">
+          <Pageing pages={data.length} setpage={setpage} activepage={page}/>
+        </div>
+        </>
+      )}
+    
   </div>
   )
   
